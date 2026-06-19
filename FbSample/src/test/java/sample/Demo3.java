56eec9ac7d86e37609884ee688fc77456d112c27
Asmita <Asmita@DESKTOP-ET1V1DV>
2026-06-19 11:04:19 +0530

    Commited

---
+++ FbSample/src/test/java/sample/Demo3.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Demo3 {
+	
+	@Test(groups = "Smoke")
+	public void sampleTest()
+	{
+		System.out.println("hi");
+	}
+
+}
---
+++ FbSample/src/test/java/sample/Demo4.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Demo4 {
+	@Test(groups = "Smoke")
+	public void sample1Test()
+	{
+		System.out.println("hi");
+	}
+
+}
